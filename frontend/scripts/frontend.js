cef9e78b2e280c828d1ba9aaea2a427b4e10238c
seleznovladislav <sandayz@mail.ru>
2022-06-21 15:46:43 +0300

    index.html is ready

--- frontend/scripts/frontend.js
+++ frontend/scripts/frontend.js
@@ -1,20 +1,30 @@
+const bar = document.querySelectorAll('.bar')	
+const radio = document.querySelectorAll('input')
+const title = document.querySelector('.h1')
+const numbers = document.querySelector('.n1') 
 function animationMain() {
-	const bar = document.querySelectorAll('.bar')	
-	const radio = document.querySelectorAll('input')
 	const arr = Array.from(radio)
-	bar[0].style.opacity = 1
-	bar[0].style.fontSize = '1.1em'
 	let i = 0
 	let memory
 	for (i; i < arr.length; i++) {
 		(function(index) {
 			arr[index].addEventListener("click", function() {
-				bar[0].style.opacity = ''
-				bar[0].style.fontSize = ''
+				bar[0].style.opacity = .6
+				bar[0].style.fontSize = '1em'
+				console.log(numbers)
 					if (memory != undefined) {
+						console.log('dede')
 						bar[memory].style.opacity = ''
 						bar[memory].style.fontSize = ''
 					}
+					if (index == 0) title.style.marginTop = '0'
+					if (index == 0) numbers.style.marginTop = '0'
+					if (index == 1) title.style.marginTop = '-160px'
+					if (index == 1) numbers.style.marginTop = '-30px'
+					if (index == 2) title.style.marginTop = '-320px'
+					if (index == 2) numbers.style.marginTop = '-60px'
+					if (index == 3) title.style.marginTop = '-480px'
+					if (index == 3) numbers.style.marginTop = '-90px'
 					bar[index].style.opacity = 1
 					bar[index].style.fontSize = '1.1em'
 					memory = index
@@ -23,3 +33,4 @@ function animationMain() {
 	}
 }
 animationMain()
+
